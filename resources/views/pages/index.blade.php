2f5b12b2c567c4e70fb442fafea9002ed84ee197
Quentin Brault <tititesouris@laposte.net>
2015-02-08 00:20:28 +0100

    Worked on home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,24 @@
 @extends('templates.starter')
 @section('content')
 <article>
-	<h1>Welcome</h1>
-	<h1>Welcome</h1>
-	<p>Welcome to OsberBot. Lorem ipsum and shit.</p>
-	<ul>{{--
-		@foreach($users as $user)
-			<li>{{ $user->id ." ". $user->username }}</li>
-		@endforeach--}}
-    </ul>
+	<section>
+		<h1>What is OsberBot</h1>
+		<div class="content">
+			<p>
+				OsberBot is a Twitch chat bot. It can do moderation tasks as well as many other things!<br />
+				Its most popular functionalities are the following.
+			</p>
+			<ul>
+				<li>
+					<span class="element">Highlights</span>The viewers notify the bot every time something they judge worth saving happens on stream.<br />
+						Afterwards, you can review the most popular moments of your streams, and decide whether or not to highlight them.
+				</li>
+				<li>
+					<span class="element">Quotes</span>OsberBot stores funny or inspiring quotes and you can fetch one back in chat at any time.<br />
+						Good for out of context jokes and remembering the good old times.
+				</li>
+			</ul>
+		</div>
+	</section>
 </article>
 @stop
